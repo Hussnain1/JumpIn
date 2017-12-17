3273e36d44ccacbf3141609752144fbd7cc945a7
Hussnain <hussnain@amiiya.com>
2017-12-17 13:20:49 +0400

    in-our-new-branch

--- src/pages/sign-up/sign-up.ts
+++ src/pages/sign-up/sign-up.ts
@@ -1,8 +1,8 @@
-import { NgForm } from '@angular/forms/';
 import { LoginPage } from './../login/login';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, LoadingController , AlertController} from 'ionic-angular';
 import { AuthJumpInService } from './../../services/authJService';
+import { NgForm } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -12,15 +12,18 @@ import { AuthJumpInService } from './../../services/authJService';
 export class SignUpPage {
   Login = LoginPage;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private authService: AuthJumpInService, private loadingCtrl: LoadingController, private alertCtrl: AlertController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private authService: AuthJumpInService, 
+    private loadingCtrl: LoadingController, private alertCtrl: AlertController) {
   }
 
   onSignUp(form: NgForm) {
-   const loading = this.loadingCtrl.create({
+    if(form.value.password == form.value.confirmPassword) {
+        const loading = this.loadingCtrl.create({
       content : 'Signing you up ...'
     });
     loading.present();
-    this.authService.signup(form.value.email, form.value.password)
+    setTimeout(() => {
+      this.authService.signup(form.value.email, form.value.password)
       .then(
         data => {
           loading.dismiss();
@@ -35,6 +38,22 @@ export class SignUpPage {
         });
         alert.present();
   });
-}
-
+    }, 3000);  
+    }
+    else {
+      const loading = this.loadingCtrl.create({
+        content : 'Signing you up ...'
+      });
+      loading.present();
+      const alert = this.alertCtrl.create({
+        title : 'Signup failed!',
+        message : 'Please put same passwords',
+        buttons: ['Try again']
+      });
+      setTimeout(() => {
+        loading.dismiss();
+        alert.present();
+      }, 3000);
+    }
+  }
 }
